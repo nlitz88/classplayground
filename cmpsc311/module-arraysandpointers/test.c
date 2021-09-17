97a7828c7803fc1c77efde9c58046d20c9cbdd56
nlitz88 <nlitz88@gmail.com>
2021-09-17 19:59:23 +0000

    Went through some different ways of stepping through arrays with pointers

--- cmpsc311/module-arraysandpointers/test.c
+++ cmpsc311/module-arraysandpointers/test.c
@@ -2,6 +2,7 @@
 // Library includes.
 #include <stdio.h>
 #include <inttypes.h>
+#include <time.h>
 
 #include "extra.h"
 
@@ -40,6 +41,52 @@ int main() {
     // Note how, because these values are passed by value, the addresses of these arguments printed
     // will be the addresses of new locations on the stack containing these arguments' values.
     showstack(3, ROWS);
+
+
+    // Iterating through an array using pointers.
+    int ages[4] = {10, 12, 20, 19};
+
+    // Iterating through using integer as iterator.
+    int i;
+    __clock_t begin = clock();
+    for(i = 0; i < 4; ++i) {
+        // IN C, address arithmetic treats literals as word address increments. In other words, saying "address + 1" is really
+        // incrementing the byte address of that variable by 1x4 addresses.
+        // printf("elem %d: %d\n", i, *(ages + i));
+        printf("Addr of ages[%d]: %p\n", i, &ages[i]);
+        // OBSERVE: Though the stack grows downwards (I.e., new elems have lower addresses),
+        //          array elems get pushed in the opposite order. In other words, the last elem gets pushed
+        //          first. This is why when we INCREMENT the base adddress of our array, so that we can
+        //          move UP the stack to the "later" elements of the array.
+    }
+    __clock_t end = clock();
+    double timeSpent1 = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("That for loop took %f seconds to complete\n", timeSpent1);
+
+    // Can also step through an array using pointers like this (more efficient).
+    // The basic idea is to keep incrementing a pointer that starts at the array's base address
+    // until that pointer is at the address after the last element.
+
+    // First, need a pointer to keep track of the end of the array so that it doesn't need to be calculated
+    // on every iteration.
+    int *oneAfterLast = ages + 4;
+    // Could also define as = &ages[4], both get the address right after the address of the last element/
+    // Then, Make a pointer that is incremented as we traverse through the stack addresses.
+    // This pointer starts at the address of ages.
+    // The condition cursor != oneAfterLast says that we can reference each item in the array, and once our cursor
+    // goes to an address after our last element, we can't go any further.
+    // Could also do something like "lastElemsAddr - Cursor >= 0", but then this introduces a more involved branch
+    // decision for each iteration == slower.
+    begin = clock();
+    for(int *cursor = ages; cursor != oneAfterLast; cursor += 1) {
+        
+        printf("Addr of Ages: %p\n", cursor);
+
+    }
+    end = clock();
+    double timeSpent2 = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("That for loop took %f seconds to complete\n", timeSpent2);
+    printf("Loop 2 was %f times faster than Loop 1\n", (double)(timeSpent1/timeSpent2));
     
     return 0;
 }
