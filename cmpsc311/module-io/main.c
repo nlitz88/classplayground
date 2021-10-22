841a6819ff763c4152f78120e923c2239e316d89
nlitz88 <nlitz88@gmail.com>
2021-10-22 19:52:59 +0000

    Added very basic file io testing

---
+++ cmpsc311/module-io/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    FILE *myFile = fopen("myData.txt", "r+");
+    char testString[128];
+    fgets(testString, 128, myFile);
+    printf(testString);
+
+    return 0;
+}
